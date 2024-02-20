1820c3ed302d1716e4e028c09df1a4465dc4d2ae
Kutay Akpınar <kutayakpnar@gmail.com>
2024-02-20 16:56:36 +0300

    CSS changes and file organization

---
+++ scripts/rps.js
@@ -0,0 +1,110 @@
+let score=JSON.parse(localStorage.getItem('score'));
+       
+if(!score){
+    score={
+        wins:0,
+        losses:0,
+        ties:0
+
+    };
+}
+
+updateScoreElements();
+
+
+function playGame(playerMove) {
+const computerMove = pickComputerMove();
+
+let result = '';
+
+if (playerMove === 'scissors') {
+  if (computerMove === 'rock') {
+    result = 'You lose.';
+  } else if (computerMove === 'paper') {
+    result = 'You win.';
+  } else if (computerMove === 'scissors') {
+    result = 'Tie.';
+  }
+
+} else if (playerMove === 'paper') {
+  if (computerMove === 'rock') {
+    result = 'You win.';
+  } else if (computerMove === 'paper') {
+    result = 'Tie.';
+  } else if (computerMove === 'scissors') {
+    result = 'You lose.';
+  }
+  
+} else if (playerMove === 'rock') {
+  if (computerMove === 'rock') {
+    result = 'Tie.';
+  } else if (computerMove === 'paper') {
+    result = 'You lose.';
+  } else if (computerMove === 'scissors') {
+    result = 'You win.';
+  }
+}
+
+if(result==="You win."){
+    score.wins++;
+
+}else if(result==="You lose."){
+    score.losses++;
+}else{
+    score.ties++;
+};
+
+localStorage.setItem('score',JSON.stringify(score));
+
+updateScoreElements();
+
+document.querySelector('.js-result').innerHTML=result;
+
+document.querySelector(".js-moves")
+  .innerHTML=  `You
+    <img src="images/${playerMove}-img.png" 
+    class="move-button">
+    <img src="images/${computerMove}-img.png" 
+    class="move-button">
+  Computer
+  `
+
+
+
+// alert(`You picked ${playerMove}. Computer picked ${computerMove}. ${result}\nWins:${score.wins} Losses:${score.losses} Ties:${score.ties}`);
+
+}
+
+function pickComputerMove() {
+const randomNumber = Math.random();
+
+let computerMove = '';
+
+if (randomNumber >= 0 && randomNumber < 1 / 3) {
+  computerMove = 'rock';
+} else if (randomNumber >= 1 / 3 && randomNumber < 2 / 3) {
+  computerMove = 'paper';
+} else if (randomNumber >= 2 / 3 && randomNumber < 1) {
+  computerMove = 'scissors';
+}
+
+return computerMove;
+}
+
+function updateScoreElements(){
+document.querySelector(".js-score")
+  .innerHTML=`Wins:${score.wins} Losses:${score.losses} Ties:${score.ties}`;
+
+}
+
+
+function resetScore(){
+score.wins=0;
+score.losses=0;
+score.ties=0;
+localStorage.removeItem('score');
+updateScoreElements();
+
+
+
+}
