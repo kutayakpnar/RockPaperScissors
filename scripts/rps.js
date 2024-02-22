8012b83557db81e1e2b81461e8d7eb13e977a6b8
Kutay Akpınar <kutayakpnar@gmail.com>
2024-02-22 16:49:16 +0300

    modification

--- scripts/rps.js
+++ scripts/rps.js
@@ -1,67 +1,86 @@
-let score=JSON.parse(localStorage.getItem('score'));
-       
-if(!score){
-    score={
-        wins:0,
-        losses:0,
-        ties:0
+let score = JSON.parse(localStorage.getItem('score'));
 
-    };
+if (!score) {
+  score = {
+    wins: 0,
+    losses: 0,
+    ties: 0
+
+  };
 }
 
 updateScoreElements();
+
+let isAutoPlaying=false;
+let intervalId;
+
+function autoPlay() {
+
+  if(!isAutoPlaying){
+    intervalId=setInterval(function () {
+      const playerMove = pickComputerMove();
+  
+      playGame(playerMove);
+  
+    }, 1000)
+    isAutoPlaying=true;
+  }else{
+    clearInterval(intervalId);
+    isAutoPlaying=false;
+  }
+}
 
 
 function playGame(playerMove) {
-const computerMove = pickComputerMove();
+  const computerMove = pickComputerMove();
 
-let result = '';
+  let result = '';
 
-if (playerMove === 'scissors') {
-  if (computerMove === 'rock') {
-    result = 'You lose.';
-  } else if (computerMove === 'paper') {
-    result = 'You win.';
-  } else if (computerMove === 'scissors') {
-    result = 'Tie.';
+  if (playerMove === 'scissors') {
+    if (computerMove === 'rock') {
+      result = 'You lose.';
+    } else if (computerMove === 'paper') {
+      result = 'You win.';
+    } else if (computerMove === 'scissors') {
+      result = 'Tie.';
+    }
+
+  } else if (playerMove === 'paper') {
+    if (computerMove === 'rock') {
+      result = 'You win.';
+    } else if (computerMove === 'paper') {
+      result = 'Tie.';
+    } else if (computerMove === 'scissors') {
+      result = 'You lose.';
+    }
+
+  } else if (playerMove === 'rock') {
+    if (computerMove === 'rock') {
+      result = 'Tie.';
+    } else if (computerMove === 'paper') {
+      result = 'You lose.';
+    } else if (computerMove === 'scissors') {
+      result = 'You win.';
+    }
   }
 
-} else if (playerMove === 'paper') {
-  if (computerMove === 'rock') {
-    result = 'You win.';
-  } else if (computerMove === 'paper') {
-    result = 'Tie.';
-  } else if (computerMove === 'scissors') {
-    result = 'You lose.';
-  }
-  
-} else if (playerMove === 'rock') {
-  if (computerMove === 'rock') {
-    result = 'Tie.';
-  } else if (computerMove === 'paper') {
-    result = 'You lose.';
-  } else if (computerMove === 'scissors') {
-    result = 'You win.';
-  }
-}
-
-if(result==="You win."){
+  if (result === "You win.") {
     score.wins++;
 
-}else if(result==="You lose."){
+  } else if (result === "You lose.") {
     score.losses++;
-}else{
+  } else {
     score.ties++;
-};
+  };
 
-localStorage.setItem('score',JSON.stringify(score));
+  localStorage.setItem('score', JSON.stringify(score));
 
-updateScoreElements();
+  updateScoreElements();
 
-document.querySelector('.js-result').innerHTML=result;
+  document.querySelector('.js-result').innerHTML = result;
 
-document.querySelector(".js-moves")
-  .innerHTML=  `You
+  document.querySelector(".js-moves")
+    .innerHTML = `You
     <img src="images/${playerMove}-img.png" 
     class="move-button">
     <img src="images/${computerMove}-img.png" 
@@ -71,39 +90,39 @@ document.querySelector(".js-moves")
 
 
 
-// alert(`You picked ${playerMove}. Computer picked ${computerMove}. ${result}\nWins:${score.wins} Losses:${score.losses} Ties:${score.ties}`);
+  // alert(`You picked ${playerMove}. Computer picked ${computerMove}. ${result}\nWins:${score.wins} Losses:${score.losses} Ties:${score.ties}`);
 
 }
 
 function pickComputerMove() {
-const randomNumber = Math.random();
+  const randomNumber = Math.random();
 
-let computerMove = '';
+  let computerMove = '';
 
-if (randomNumber >= 0 && randomNumber < 1 / 3) {
-  computerMove = 'rock';
-} else if (randomNumber >= 1 / 3 && randomNumber < 2 / 3) {
-  computerMove = 'paper';
-} else if (randomNumber >= 2 / 3 && randomNumber < 1) {
-  computerMove = 'scissors';
+  if (randomNumber >= 0 && randomNumber < 1 / 3) {
+    computerMove = 'rock';
+  } else if (randomNumber >= 1 / 3 && randomNumber < 2 / 3) {
+    computerMove = 'paper';
+  } else if (randomNumber >= 2 / 3 && randomNumber < 1) {
+    computerMove = 'scissors';
+  }
+
+  return computerMove;
 }
 
-return computerMove;
-}
-
-function updateScoreElements(){
-document.querySelector(".js-score")
-  .innerHTML=`Wins:${score.wins} Losses:${score.losses} Ties:${score.ties}`;
+function updateScoreElements() {
+  document.querySelector(".js-score")
+    .innerHTML = `Wins:${score.wins} Losses:${score.losses} Ties:${score.ties}`;
 
 }
 
 
-function resetScore(){
-score.wins=0;
-score.losses=0;
-score.ties=0;
-localStorage.removeItem('score');
-updateScoreElements();
+function resetScore() {
+  score.wins = 0;
+  score.losses = 0;
+  score.ties = 0;
+  localStorage.removeItem('score');
+  updateScoreElements();
 
 
 
--- scripts/todo-list.js
+++ scripts/todo-list.js
@@ -1,31 +1,76 @@
-const todoList=[];
+const todoList = [{
+    name: "make dinner",
+    dueDate: "2022-12-22"
+}, {
+    name: "wash dishes",
+    dueDate: "2022-12-10"
 
-updateTodos();
 
-function addTodo(){
-    const inputElement=document.querySelector('.js-name-input');
-    const name=inputElement.value;
-    todoList.push(name);
-    console.log(todoList);
-    inputElement.value='';
-    updateTodos();
-    
+}];
+
+renderTodos();
+
+function addTodo() {
+    const inputElement = document.querySelector('.js-name-input');
+    const name = inputElement.value;
+    const dateInputElement = document.querySelector('.js-due-date');
+    const dueDate = dateInputElement.value;
+    todoList.push({
+        name: name,
+        dueDate: dueDate
+    });
+    //console.log(todoList);
+    inputElement.value = '';
+    renderTodos();
+
 
 }
 
 
 
-function updateTodos(){
-    let todoListHTML=''
+function renderTodos() {
+    let todoListHTML = ''
 
-    for(let i=0;i<todoList.length;i++){
-        const todo=todoList[i];
-        const html=`<p>${todo}</p>`;
-        todoListHTML+=html;
+    todoList.forEach(function(todoObject,index){
+        //const name=todoObject.name;
+        const { name, dueDate } = todoObject;
+        //const {dueDate}=todoObject;
+        //const dueDate=todoObject.dueDate;
+
+        const html = `
+        <div>${name}</div>
+        <div> ${dueDate}</div>
+         <button onclick="
+            todoList.splice(${index},1);
+            renderTodos();
+         " class="delete-todo-button">Delete</button>
+         `;
+        todoListHTML += html;
+
+    }
+)
+/*
+    for (let i = 0; i < todoList.length; i++) {
+        const todoObject = todoList[i];
+        //const name=todoObject.name;
+        const { name, dueDate } = todoObject;
+        //const {dueDate}=todoObject;
+        //const dueDate=todoObject.dueDate;
+
+        const html = `
+        <div>${name}</div>
+        <div> ${dueDate}</div>
+         <button onclick="
+            todoList.splice(${index},1);
+            renderTodos();
+         " class="delete-todo-button">Delete</button>
+         `;
+        todoListHTML += html;
 
 
     }
-    document.querySelector('.todos').innerHTML=todoListHTML;
+    */
+    document.querySelector('.todos').innerHTML = todoListHTML;
 
 
 }
